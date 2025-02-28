0d5df42db0b9a2227f16ec25a37657e02812e7da
MinJunChoi <chlalswns200@naver.com>
2025-02-28 16:50:03 +0900

    add member api saveMember V2

--- src/main/java/jpabook/jpashop/api/MemberApiController.java
+++ src/main/java/jpabook/jpashop/api/MemberApiController.java
@@ -5,11 +5,10 @@ import jpabook.jpashop.service.MemberService;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
+import javax.validation.constraints.NotNull;
 
 @RestController
 @RequiredArgsConstructor
@@ -18,7 +17,16 @@ public class MemberApiController {
     private final MemberService memberService;
 
     @PostMapping("/api/v1/members")
-    public CreateMemberResponse saveMemberV1(@RequestParam @Valid Member member) {
+    public CreateMemberResponse saveMemberV1(@RequestBody @Valid Member member) {
+        Long id = memberService.join(member);
+        return new CreateMemberResponse(id);
+    }
+
+    @PostMapping("/api/v2/members")
+    public CreateMemberResponse saveMemberV2(@RequestBody @Valid CreateMemberRequest request) {
+        Member member = new Member();
+        member.setName(request.getName());
+
         Long id = memberService.join(member);
         return new CreateMemberResponse(id);
     }
