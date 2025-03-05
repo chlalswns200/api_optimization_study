d1e55700c99c24d09db3b0357df3761a22da6ed6
MinJunChoi <chlalswns200@naver.com>
2025-03-05 16:23:22 +0900

    add order api findOrders(jpa to dto) V4

--- src/main/java/jpabook/jpashop/api/OrderSimpleApiController.java
+++ src/main/java/jpabook/jpashop/api/OrderSimpleApiController.java
@@ -5,6 +5,8 @@ import jpabook.jpashop.domain.Order;
 import jpabook.jpashop.domain.OrderStatus;
 import jpabook.jpashop.repository.OrderRepository;
 import jpabook.jpashop.repository.OrderSearch;
+import jpabook.jpashop.repository.order.simplequery.OrderSimpleQueryDto;
+import jpabook.jpashop.repository.order.simplequery.OrderSimpleQueryRepository;
 import lombok.Data;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -19,6 +21,7 @@ import java.util.stream.Collectors;
 public class OrderSimpleApiController {
 
     private final OrderRepository orderRepository;
+    private final OrderSimpleQueryRepository orderSimpleQueryRepository;
 
     /**
      * V1. 엔티티 직접 노출
@@ -53,6 +56,11 @@ public class OrderSimpleApiController {
         return collect;
     }
 
+    @GetMapping("/api/v4/simple-orders")
+    public List<OrderSimpleQueryDto> ordersV4() {
+        return orderSimpleQueryRepository.findOrderDtos();
+    }
+
 
     @Data
     static class SimpleOrderDto {
@@ -61,6 +69,7 @@ public class OrderSimpleApiController {
         private LocalDateTime orderDate; //주문시간
         private OrderStatus orderStatus;
         private Address address;
+
         public SimpleOrderDto(Order order) {
             orderId = order.getId();
             name = order.getMember().getName();
